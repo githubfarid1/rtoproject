787f303d926009b601c30622eb049ba1eed5c252
Mohamad Farid Saleh <github.farid1@gmail.com>
2024-05-14 11:54:33 +0900

    update scripts

---
+++ getseekurls.py
@@ -0,0 +1,237 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from datetime import datetime, timedelta
+import time
+import sys
+from itertools import groupby
+from operator import itemgetter
+import json
+import setting as s
+import os
+
+getheaders = {
+    'authority': 'www.seek.com.au',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9,id;q=0.8',
+    'cache-control': 'max-age=0',
+    'referer': 'https://www.upwork.com/',
+    'sec-ch-ua': '"Not A(Brand";v="99", "Microsoft Edge";v="121", "Chromium";v="121"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'cross-site',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0',
+}
+response = requests.get('https://www.seek.com.au', headers=getheaders)
+cookies_dict = response.cookies.get_dict()
+cookies = cookies_dict
+# breakpoint()
+subclass = s.SUBCLASSIFICATIONS
+alldata = []
+page = 0
+urls = []
+maxcount = 550
+for cl in subclass:
+    params = {
+        'subclassification': cl, 
+    }
+    response = requests.get('https://www.seek.com.au/jobs-in-education-training', params=params, cookies=cookies, headers=getheaders)
+    html = response.content
+    soup = BeautifulSoup(html, "html.parser")
+    # breakpoint()
+    try:
+        if "," in soup.find('span', {"data-automation":"totalJobsCount"}).text:
+            jobcount = int(soup.find('span', {"data-automation":"totalJobsCount"}).text.replace(",",""))
+        else:
+            jobcount = int(soup.find('span', {"data-automation":"totalJobsCount"}).text)
+    except:
+        jobcount = 0
+
+    worktype = [242,243,244,245]
+    if jobcount > maxcount:
+        for wt in worktype:
+            params = {
+                'subclassification': cl,
+                'worktype': wt, 
+            }
+            response = requests.get('https://www.seek.com.au/jobs-in-education-training', params=params, cookies=cookies, headers=getheaders)
+            html = response.content
+            soup = BeautifulSoup(html, "html.parser")
+            try:
+                if "," in soup.find('span', {"data-automation":"totalJobsCount"}).text:
+                    jobcount = int(soup.find('span', {"data-automation":"totalJobsCount"}).text.replace(",",""))
+                else:
+                    jobcount = int(soup.find('span', {"data-automation":"totalJobsCount"}).text)
+            except:
+                jobcount = 0
+            salaryrange = ['0-30000', '30000-40000', '40000-50000', '50000-60000', '60000-70000', '70000-80000', '80000-100000', '100000-120000', '120000-150000', '150000-200000', '200000-250000', '250000-350000', '350000-']
+            if jobcount > maxcount:
+                for sal in salaryrange:
+                    params = {
+                        'subclassification': cl,
+                        'worktype': wt,
+                        'salaryrange': sal 
+                    }
+                    
+                    response = requests.get('https://www.seek.com.au/jobs-in-education-training', params=params, cookies=cookies, headers=getheaders)
+                    html = response.content
+                    soup = BeautifulSoup(html, "html.parser")
+                    try:
+                        if "," in soup.find('span', {"data-automation":"totalJobsCount"}).text:
+                            jobcount = int(soup.find('span', {"data-automation":"totalJobsCount"}).text.replace(",",""))
+                        else:
+                            jobcount = int(soup.find('span', {"data-automation":"totalJobsCount"}).text)
+                    except:
+                        jobcount = 0
+
+                    if jobcount > maxcount:
+
+                        #---
+                        daterange = 7
+                        params = {
+                            'subclassification': cl,
+                            'worktype': wt,
+                            'salaryrange': sal,
+                            'daterange': daterange 
+                        }
+                        
+                        response = requests.get('https://www.seek.com.au/jobs-in-education-training', params=params, cookies=cookies, headers=getheaders)
+                        html = response.content
+                        soup = BeautifulSoup(html, "html.parser")
+                        try:
+                            if "," in soup.find('span', {"data-automation":"totalJobsCount"}).text:
+                                jobcount = int(soup.find('span', {"data-automation":"totalJobsCount"}).text.replace(",",""))
+                            else:
+                                jobcount = int(soup.find('span', {"data-automation":"totalJobsCount"}).text)
+                        except:
+                            jobcount = 0
+
+                        if jobcount > maxcount:
+                            print(jobcount, 'out')
+                        # else:
+                        print(jobcount, 'subclassification', cl, 'worktype', wt, "salary", sal, "daterange", daterange)
+                        # breakpoint()
+                        page = 0
+                        while True:
+                            page += 1
+                            params = {
+                                'page': page,
+                                'subclassification': cl, 
+                                'worktype': wt,
+                                'salaryrange': sal,
+                                'daterange': daterange 
+                            }
+                            response = requests.get('https://www.seek.com.au/jobs-in-education-training', params=params, cookies=cookies, headers=getheaders)
+                            html = response.content
+                            soup = BeautifulSoup(html, "html.parser")
+            
+                            print("page", page)
+
+                            trs = soup.find_all("article", class_='y735df0')
+                            print(len(trs))
+                            if len(trs) == 0:
+                                break    
+                            # dlist = []
+                            
+                            for tr in trs:
+                                atl = tr.find("a", {"data-automation":"job-list-view-job-link"}).get('href')
+                                atl =  atl.split('?')[0].replace("/job/","")
+                                joblink = atl
+                                urls.append(joblink)
+
+                        #---
+
+
+                    else:
+                        print(jobcount, 'subclassification', cl, 'worktype', wt, "salary", sal)
+                        # breakpoint()
+                        page = 0
+                        while True:
+                            page += 1
+                            params = {
+                                'page': page,
+                                'subclassification': cl, 
+                                'worktype': wt,
+                                'salaryrange': sal 
+                            }
+                            response = requests.get('https://www.seek.com.au/jobs-in-education-training', params=params, cookies=cookies, headers=getheaders)
+                            html = response.content
+                            soup = BeautifulSoup(html, "html.parser")
+             
+                            print("page", page)
+
+                            trs = soup.find_all("article", class_='y735df0')
+                            print(len(trs))
+                            if len(trs) == 0:
+                                break    
+                            # dlist = []
+                            
+                            for tr in trs:
+                                atl = tr.find("a", {"data-automation":"job-list-view-job-link"}).get('href')
+                                atl =  atl.split('?')[0].replace("/job/","")
+                                joblink = atl
+                                urls.append(joblink)
+
+
+            else:
+                print(jobcount, 'subclassification', cl, 'worktype', wt)
+                page = 0
+                while True:
+                    page += 1
+                    params = {
+                        'page': page,
+                        'subclassification': cl, 
+                        'worktype': wt, 
+                    }
+                    response = requests.get('https://www.seek.com.au/jobs-in-education-training', params=params, cookies=cookies, headers=getheaders)
+                    html = response.content
+                    soup = BeautifulSoup(html, "html.parser")
+
+                    print("page", page)
+
+                    trs = soup.find_all("article", class_='y735df0')
+                    if len(trs) == 0:
+                        break    
+                    # dlist = []
+                    
+                    for tr in trs:
+                        atl = tr.find("a", {"data-automation":"job-list-view-job-link"}).get('href')
+                        atl =  atl.split('?')[0].replace("/job/","")
+                        joblink = atl
+                        urls.append(joblink)
+
+    else:
+        print(jobcount, 'subclassification', cl)
+        page = 0
+        while True:
+            page += 1
+            params = {
+                'page': page,
+                'subclassification': cl, 
+            }
+            response = requests.get('https://www.seek.com.au/jobs-in-education-training', params=params, cookies=cookies, headers=getheaders)
+            print("page", page)
+            html = response.content
+            soup = BeautifulSoup(html, "html.parser")
+
+            trs = soup.find_all("article", class_='y735df0')
+            if len(trs) == 0:
+                break    
+            # dlist = []
+            
+            for tr in trs:
+                atl = tr.find("a", {"data-automation":"job-list-view-job-link"}).get('href')
+                atl =  atl.split('?')[0].replace("/job/","")
+                joblink = atl
+                urls.append(joblink)
+        
+urlsset = set(urls)
+urls = list(urlsset)
+print("Found urls:", len(urls))
+
+with open(s.RESFOLDER + os.path.sep + "urls.json", "w") as f:
+    json.dump(urls, f)
---
+++ setting.py
@@ -0,0 +1,22 @@
+"""
+    6124: Childcare & Outside School Hours Care
+    6125: Library Services & Information Management
+    6126: Management - Schools
+    6127: Management - Universities
+    6128: Management - Vocational
+    6129: Research & Fellowships
+    6130: Student Services
+    6132: Teaching - Early Childhood
+    6133: Teaching - Primary
+    6134: Teaching - Secondary
+    6135: Teaching - Tertiary
+    6136: Teaching - Vocational
+    6131: Teaching Aides & Special Needs
+    6137: Tutoring
+    6138: Workplace Training & Assessment
+    6139: Other 
+"""
+# SUBCLASSIFICATIONS = [6124, 6125, 6126, 6127, 6128, 6129, 6130, 6132, 6133, 6134, 6135, 6136, 6131, 6137, 6138, 6139]
+SUBCLASSIFICATIONS = [6124]
+
+RESFOLDER = 'fileresult'
